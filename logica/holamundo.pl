36fdea0c8aeeceb0182a22a17db7b7183b2ad95d
German Trevizo <69019198+German-Trevizo@users.noreply.github.com>
2021-01-22 17:17:35 -0700

    introduccion a Prolog

---
+++ logica/holamundo.pl
@@ -0,0 +1,13 @@
+/* primero agregamos echos, terminamos la funcion con un . 
+*/
+valioso(oro).
+valioso(cobre).
+es_libro(cienanios).
+
+progenitor(clara,jose).
+progenitor(tomas,jose).
+progenitor(tomas,isabel).
+progenitor(jose,ana).
+progenitor(jose,patricia).
+progenitor(praticia,jaime).
+
